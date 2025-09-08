a759f4e5f9ac33311bf60adbd915b654bb6176ff
Arghya Ghosh <71373838+uiuxarghya@users.noreply.github.com>
2025-09-08 22:45:07 +0530

    feat(app): add a minimal homepage

--- app/index.tsx
+++ app/index.tsx
@@ -3,8 +3,11 @@ import { Text, View } from "react-native";
 export default function Index() {
   return (
     <View className="flex-1 items-center justify-center">
-      <Text className="text-5xl leading-[50px] font-bold font-space-mono text-blue-700 dark:text-red-500">
+      <Text className="text-5xl leading-[50px] font-bold font-space-mono text-blue-700">
         AquaSetu
+      </Text>
+      <Text className="mt-3 text-center text-lg max-w-sm font-space-mono text-gray-700">
+        Your companion for groundwater quality monitoring and management.
       </Text>
     </View>
   );
