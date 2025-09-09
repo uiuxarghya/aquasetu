544a6bb33759b0ba31f42a9f3a86d65a5288defd
HikariZasureiya <info.d.podder@gmail.com>
2025-09-09 08:33:15 +0530

    (feat): added basic auth with email and password and Oauth with google

--- app/index.tsx
+++ app/index.tsx
@@ -1,11 +1,66 @@
-import { Text, View } from "react-native";
+import client from "@/appWriteConfig";
+import { getLoginStatus, logout } from "@/utils/authUtils";
+import { useRouter } from "expo-router";
+import { useEffect, useState } from "react";
+import { Button, Text, View } from "react-native";
+import { Account } from "react-native-appwrite";
+let account = new Account(client);
+
+// init router
+const router = useRouter();
 
 export default function Index() {
+  const [loginStatus, setLoginStatus] = useState(false);
+  
+  //check if logged in >.<
+  useEffect(() => {
+    const loginstatusfun = async () => {
+      const loginstat = await getLoginStatus(account);
+      setLoginStatus(loginstat);
+    };
+
+    loginstatusfun();
+  }, []);
+
   return (
     <View className="flex-1 items-center justify-center">
       <Text className="text-5xl leading-[50px] font-bold font-space-mono text-blue-700 dark:text-red-500">
         AquaSetu
       </Text>
+
+
+      {loginStatus ? (
+        <View className="mt-5">
+          <Button
+            title="Logout"
+            onPress={async () => {
+              const res = await logout(account);
+              if (res) {
+                setLoginStatus(false);
+              }
+            }}
+          />
+        </View>
+      ) : (
+        <View>
+          <View className="mb-5 mt-5">
+            <Button
+              title="Register"
+              onPress={() => {
+                router.push("/(auth)/register");
+              }}
+            />
+          </View>
+          <View className="mb-5 mt-5">
+            <Button
+              title="Login"
+              onPress={() => {
+                router.push("/(auth)/login");
+              }}
+            />
+          </View>
+        </View>
+      )}
     </View>
   );
 }
---
+++ appWriteConfig.ts
@@ -0,0 +1,10 @@
+import {Client} from 'react-native-appwrite';
+
+let client: Client = new Client();  
+client
+    .setEndpoint(process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT ??  "http://localhost/v1")
+    .setProject(process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID!) 
+    .setPlatform(process.env.EXPO_PUBLIC_APPWRITE_PROJECT_NAME!);
+
+export default client;
+
