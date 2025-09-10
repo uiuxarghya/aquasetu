d97c7256009929ec537601e79d4eb1573191066a
Arghya Ghosh <71373838+uiuxarghya@users.noreply.github.com>
2025-09-10 20:29:24 +0530

    refactor: remove unused Tailwind CSS plugin and dotenv configuration from Babel config

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,3 @@
-const plugin = require("tailwindcss");
-
 module.exports = function (api) {
   api.cache(true);
   return {
@@ -7,17 +5,5 @@ module.exports = function (api) {
       ["babel-preset-expo", { jsxImportSource: "nativewind" }],
       "nativewind/babel",
     ],
-    plugins: [
-      [
-        "module:react-native-dotenv", {
-          "moduleName": "@env",
-          "path": ".env",
-          "blacklist": null,
-          "whitelist": null,
-          "safe": false,
-          "allowUndefined": true
-        }
-      ]
-    ]
   };
 };
