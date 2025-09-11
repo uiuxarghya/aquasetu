51ad2ca8557304a35694ace0cc7a8762621efeba
Arghya Ghosh <71373838+uiuxarghya@users.noreply.github.com>
2025-09-11 10:03:25 +0530

    feat(metro): add inlineRem option to NativeWind configuration

--- metro.config.js
+++ metro.config.js
@@ -3,4 +3,7 @@ const { withNativeWind } = require("nativewind/metro");
 
 const config = getDefaultConfig(__dirname);
 
-module.exports = withNativeWind(config, { input: "./app/globals.css" });
+module.exports = withNativeWind(config, {
+  input: "./app/globals.css",
+  inlineRem: 16,
+});
